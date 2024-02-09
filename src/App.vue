fd4b7a253e3d73557d8edd38526215be97624230
SREYPHEAP.KHIN <SREYPHEAP.KHIN@PNC.LAN>
2024-02-09 10:10:52 +0700

    [Setup] setup project environment

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1 class="text-3xl text-center my-8 font-bold text-pink-600">
+    Countries Catalog Implementation
+  </h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
